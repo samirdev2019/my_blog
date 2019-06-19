e0f4c638562e17ecfaf7c9094f0e7d2a3614dcbc
Samir <allabsamir777@gmail.com>
2019-06-19 11:39:27 +0200

    add the comments controller and model

---
+++ controler/CommentController.php
@@ -0,0 +1,42 @@
+<?php
+require '../model/CommentManager.php';
+class CommentController{
+    private $db;
+
+    /**
+     * __construct
+     * pdo initialisation 
+     * @return void
+     */
+    public function __construct(){
+        $this->db = new CommentManager('mybolg');
+    }
+    /**
+     * getComments
+     *this function receives in parameter the post identifier and request the model
+     * for their validated comments
+     * @param  int $id the post identifier
+     *
+     * @return array objects array of validated comments
+     */
+    public function getComments(int $id): array{
+        $id = (int) $id;
+        if($id > 0){
+            return $this->db->getComments($id);
+        }else{
+            require '../public/index.php?p=posts';
+        }
+    }
+    /**
+     * addComment
+     * this function allows to add a comment
+     * @param  mixed $postId the post identifier
+     * @param  mixed $userId the user identifier
+     * @param  mixed $content the user comment to send
+     *
+     * @return void
+     */
+    public function addComment(int $postId,int $userId,string $content): void{
+        $this->db->addComment($postId,$userId,$content);
+    }
+}
--- model/CommentManager.php
+++ model/CommentManager.php
@@ -1,4 +1,45 @@
 <?php
-class Comment {
+//require 'Database.php';
+/**
+ * this class allows to manage the comments sent by the users
+ */
+class CommentManager extends Database{
+    private $pdo;
+    public function __construct(){
+        $this->pdo = $this->getPDO();
+    }
     
+    /**
+     * getComments
+     *this function receives in parameter the identifier of the post and returns their validated comments
+     * @param  int $id the identifier of the post
+     *
+     * @return object validated comments
+     */
+    public function getComments($id){
+        $comments=$this->pdo->prepare('SELECT comment_id,content,comments.validated,post_id,date_format(commented,\' %d\%m\%Y à %Hh%imin%ss\') as coment_date,users.username
+        FROM comments INNER JOIN users ON comments.user_id = users.user_id WHERE post_id = ? AND comments.validated is not null
+         ORDER BY coment_date DESC');
+        $comments->execute(array($id));
+        return $comments->fetchAll(PDO::FETCH_OBJ);
+        
+    }  
+    /**
+     * addComment
+     * this function allows to add a comment
+     * @param  int $postId
+     * @param  int $userId
+     * @param  string $content
+     *
+     * @return boolean true if the insertion affected
+     */
+    public function addComment(int $postId,int $userId,string $content): boolean {
+        $comment=$this->pdo->prepare('INSERT INTO comments(commented,content,post_id,user_id) VALUES (now(),:content,:post_id,:user_id)');
+		$comment->execute(array(
+			'post_id' => $postId,
+			'user_id' =>$userId,
+			'content' =>$content
+		));
+		return $comment;
+    } 
 }
